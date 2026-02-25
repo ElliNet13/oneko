d6bc31c8e405859e66ce40845b9a4c55d597dc97
ElliNet13 (PC) <elliotmoose4@gmail.com>
2026-02-24 19:55:33 -0700

    Add reduced motion detection back

--- oneko.js
+++ oneko.js
@@ -1,6 +1,12 @@
 // oneko.js: https://github.com/adryd325/oneko.js
 
 (async function oneko() {
+  const isReducedMotion =
+    window.matchMedia(`(prefers-reduced-motion: reduce)`) === true ||
+    window.matchMedia(`(prefers-reduced-motion: reduce)`).matches === true;
+
+  if (isReducedMotion) return;
+  
   const nekoEl = document.createElement("div");
   let nekoPosX = 32,
     nekoPosY = 32,
