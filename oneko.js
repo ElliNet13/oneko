6f52158b4eb9a2931b2ce7f0a3513d423f2d1b20
ElliNet13 (PC) <elliotmoose4@gmail.com>
2026-02-25 20:01:00 -0700

    Update version number for "Use unsafeWindow to check for onekoInterval"

--- oneko.js
+++ oneko.js
@@ -2,7 +2,7 @@
 // @name            Oneko
 // @namespace       https://ellinet13.github.io
 // @match           *://*/*
-// @version         2.0
+// @version         2.1
 // @author          ElliNet13
 // @description     cat follow mouse
 // @downloadURL     https://ellinet13.github.io/oneko/oneko.js
